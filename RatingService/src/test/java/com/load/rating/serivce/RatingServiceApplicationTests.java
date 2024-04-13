9591a7d925f00ea8352c6a3db8c40afc601ae7ec
amarnath pandit <amarnathpandit722@gmail.com>
2024-04-13 16:44:40 +0530

    Deploy commit

--- RatingService/src/test/java/com/load/rating/serivce/RatingServiceApplicationTests.java
+++ RatingService/src/test/java/com/load/rating/serivce/RatingServiceApplicationTests.java
@@ -7,7 +7,17 @@ import org.springframework.boot.test.context.SpringBootTest;
 class RatingServiceApplicationTests {
 
 	@Test
-	void contextLoads() {
-	}
+    void contextLoads() {
+        try {
+            // Add any additional debugging information here
+            System.out.println("Attempting to load application context...");
+            // Call the Spring Boot application context loading code here
+            // For example: SpringApplication.run(RatingServiceApplication.class);
+        } catch (Exception ex) {
+        	System.out.println("Attempting to load application context error solving...");
+            // Log any exceptions that occur during context loading
+            ex.printStackTrace();
+        }
+    }
 
 }
